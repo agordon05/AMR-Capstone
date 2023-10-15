25d926cb4e121a2ae6eceb64a928a701b5da27b7
James Perry <jp28543@georgiasouthern.edu>
2023-10-15 19:22:46 -0400

    Made basic program for assigning and visualizing camera vision.

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -5,3 +5,11 @@ import cv2
 import pylibdmtx #https://libdmtx.sourceforge.net/
 from pylibdmtx.pylibdmtx import decode #as dmtxdecode
 
+capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = capture.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+capture.release()
