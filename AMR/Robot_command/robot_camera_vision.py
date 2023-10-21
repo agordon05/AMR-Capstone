22dfdc5a86d87b5109d745a81869de7337d0b581
James Perry <jp28543@georgiasouthern.edu>
2023-10-21 17:18:21 -0400

    removed pylibdmtx imports; caused error for some reason

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -2,8 +2,8 @@ import os
 import sys
 import numpy
 import cv2
-import pylibdmtx #https://libdmtx.sourceforge.net/
-from pylibdmtx.pylibdmtx import decode #as dmtxdecode
+#import pylibdmtx #https://libdmtx.sourceforge.net/
+#from pylibdmtx.pylibdmtx import decode #as dmtxdecode
 
 capture = cv2.VideoCapture(0)
 
