dc0e2c253c334b9bd8bf9bc63994d02a3b813595
agordon05 <100168379+agordon05@users.noreply.github.com>
2023-10-24 18:35:40 -0400

    Reorganized RobotService class

--- server/AMRCapstone/src/main/java/com/AMRCapstone/AMRCapstone/service/RobotService.java
+++ server/AMRCapstone/src/main/java/com/AMRCapstone/AMRCapstone/service/RobotService.java
@@ -15,6 +15,7 @@ import com.AMRCapstone.AMRCapstone.model.Robot;
 
 @Service
 public class RobotService {
+
     public Robot get() {
         Robot robot = RobotAccess.getRobot();
         if (robot == null) {
@@ -25,12 +26,105 @@ public class RobotService {
     }
 
     public ResponseEntity<float[]> update(Robot temp) {
+
         Robot robot = RobotAccess.getRobot();
         if (robot == null) {
             RobotAccess.initialize();
             robot = RobotAccess.getRobot();
         }
         // validation
+        validateAndUpdateRobot(robot, temp);
+        // update
+        HttpStatus status = getQRStatus(robot);
+        updateQRQueue(status, robot);
+        updateDestination(robot);
+
+        // create a response
+        float[] response = generateResponse(status, robot);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return new ResponseEntity<float[]>(response, headers, HttpStatus.OK);
+    }
+
+    private void updateDestination(Robot robot) {
+        robot.setX_destination(QR_Queue.getCurrentQR().getX_pos());
+        robot.setY_destination(QR_Queue.getCurrentQR().getY_pos());
+        robot.setQrScan(null);
+    }
+
+    private float[] generateResponse(HttpStatus status, Robot robot) {
+        float[] response;
+
+        if (status.equals(HttpStatus.ACCEPTED) || status.equals(HttpStatus.CONTINUE)) {
+            response = new float[4];
+
+            // destination
+            response[0] = robot.getX_destination();
+            response[1] = robot.getY_destination();
+            if (QR_Queue.getPrevQR() != null) {
+                // QR position
+                response[2] = QR_Queue.getPrevQR().getX_pos();
+                response[3] = QR_Queue.getPrevQR().getY_pos();
+            }
+
+        }
+
+        else {
+            response = new float[2];
+            response[0] = robot.getX_destination();
+            response[1] = robot.getY_destination();
+
+        }
+        return response;
+    }
+
+    private HttpStatus getQRStatus(Robot robot) {
+
+        if (robot.getQrScan() == null) {
+            return HttpStatus.OK;
+        }
+        // test QR Scan
+        else {
+            // check QRQueue
+            if (QR_Queue.getCurrentQR().getCode().equals(robot.getQrScan())) {
+                return HttpStatus.CONTINUE;
+
+            } else if (QRAccess.getQrByQRCode(robot.getQrScan()) != null) {
+                // reset Queue
+                QR resetQR = QRAccess.getQrByQRCode(robot.getQrScan());
+
+                if (resetQR == null) {
+                    return HttpStatus.CONFLICT;
+                } else {
+                    return HttpStatus.ACCEPTED;
+                }
+
+            } else {
+                return HttpStatus.NOT_FOUND;
+            }
+        }
+    }
+
+    private void updateQRQueue(HttpStatus status, Robot robot) {
+
+        if (status.equals(HttpStatus.CONTINUE)) {
+            // pop queue
+            QR_Queue.pop();
+            QR_Queue.QueueUp();
+
+        } else if (status.equals(HttpStatus.ACCEPTED)) {
+            // reset and pop queue
+            QR resetQR = QRAccess.getQrByQRCode(robot.getQrScan());
+            QR_Queue.resetQueue(resetQR);
+            QR_Queue.pop();
+            QR_Queue.QueueUp();
+        }
+
+    }
+
+    private void validateAndUpdateRobot(Robot robot, Robot temp) {
         // check status
         if (temp.getStatus().equals(Codes.statusActive) || temp.getStatus().equals(Codes.statusInactive)) {
             robot.setStatus(temp.getStatus());
@@ -61,84 +155,6 @@ public class RobotService {
         robot.setQrScan(temp.getQrScan());
         robot.addtoLoggerList(temp.getLoggerList());
         robot.setImage(temp.getImage());
-
-        HttpStatus status;
-
-        if (robot.getQrScan() == null) {
-            status = HttpStatus.OK;
-        }
-        // test QR Scan
-        else {
-            // check QRQueue
-            if (QR_Queue.getCurrentQR().getCode().equals(robot.getQrScan())) {
-                // pop queue
-                QR_Queue.pop();
-                QR_Queue.QueueUp();
-
-                robot.setQrScan(null);
-                status = HttpStatus.CONTINUE;
-
-            } else if (QRAccess.getQrByQRCode(robot.getQrScan()) != null) {
-                // reset Queue
-                QR resetQR = QRAccess.getQrByQRCode(robot.getQrScan());
-                if (resetQR == null) {
-                    status = HttpStatus.CONFLICT;
-                } else {
-                    QR_Queue.resetQueue(resetQR);
-                    QR_Queue.pop();
-                    QR_Queue.QueueUp();
-                    robot.setQrScan(null);
-                    // send out destination, QR position, Http status I-am-a-tea-pot
-                    status = HttpStatus.ACCEPTED;
-                }
-
-            } else {
-                status = HttpStatus.NOT_FOUND;
-            }
-
-        }
-        // //ensures
-        // if (QR_Queue.getPrevQR() != null) {
-        // while (QR_Queue.getCurrentQR().getX_pos() == QR_Queue.getPrevQR().getX_pos()
-        // && QR_Queue.getCurrentQR().getY_pos() == QR_Queue.getPrevQR().getY_pos()) {
-        // QR_Queue.pop();
-        // QR_Queue.QueueUp();
-        // }
-        // }
-
-        robot.setX_destination(QR_Queue.getCurrentQR().getX_pos());
-        robot.setY_destination(QR_Queue.getCurrentQR().getY_pos());
-        robot.setQrScan(null);
-
-        float[] response;
-
-        if (status.equals(HttpStatus.ACCEPTED) || status.equals(HttpStatus.CONTINUE)) {
-            response = new float[4];
-
-            // destination
-            response[0] = robot.getX_destination();
-            response[1] = robot.getY_destination();
-            if (QR_Queue.getPrevQR() != null) {
-                // QR position
-                response[2] = QR_Queue.getPrevQR().getX_pos();
-                response[3] = QR_Queue.getPrevQR().getY_pos();
-            }
-
-        }
-
-        else {
-            response = new float[2];
-            response[0] = robot.getX_destination();
-            response[1] = robot.getY_destination();
-
-        }
-
-        // create response
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        return new ResponseEntity<float[]>(response, headers, HttpStatus.OK);
     }
 
     public String getUserControl() {
