5e86691524901df0e63ef4b57781661514998e93
James Perry <jp28543@georgiasouthern.edu>
2023-10-26 19:07:54 -0400

    implemented jupyterlab camera instance creation

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -9,6 +9,7 @@ from jetbot import Camera, bgr8_to_jpeg
 
 camera = Camera.instance(width=224, height=224)
 
-image = widgets.Image(format='jpeg', width=224, height=224)  # this width and height doesn't necessarily have to match the camera
+image = widgets.Image(format='jpeg', width=224, height=224)  # this width and height doesn't necessarily
+                                                             # have to match the camera
 
 camera_link = traitlets.dlink((camera, 'value'), (image, 'value'), transform=bgr8_to_jpeg)
