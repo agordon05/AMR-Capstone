c0f2cb9f7935d812c18ffcd2761764b94ec6ba2c
James Perry <jp28543@georgiasouthern.edu>
2023-10-27 17:38:55 -0400

    camera instantiated

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -1,15 +1,17 @@
 import os
 import sys
+from AMR.Robot_command import robot_startup
 import numpy
 import cv2
 import traitlets
 import ipywidgets.widgets as widgets
-from IPython.display import display
-from jetbot import Camera, bgr8_to_jpeg #try jetcam import for reading images
+
+directory = robot_startup.directory
+sys.path.append(directory)
+
+from jetbot import Camera, bgr8_to_jpeg
 
 camera = Camera.instance(width=224, height=224)
-
-image = widgets.Image(format='jpeg', width=224, height=224)  # this width and height doesn't necessarily
-                                                             # have to match the camera
+image = widgets.Image(format='jpeg', width=224, height=224)  # this width and height doesn't necessarily have to match the camera
 
 camera_link = traitlets.dlink((camera, 'value'), (image, 'value'), transform=bgr8_to_jpeg)
