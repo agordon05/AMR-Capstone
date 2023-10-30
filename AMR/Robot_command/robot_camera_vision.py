5497c1fef8d9106590793afcd291d83ff27842f6
agordon05 <100168379+agordon05@users.noreply.github.com>
2023-10-30 18:27:58 -0400

    Update robot_camera_vision.py

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -1,11 +1,15 @@
 import os
 import sys
-import robot_startup
+#import robot_startup
 #from robot_startup import robot
 import numpy
 import cv2
 import traitlets
 import ipywidgets.widgets as widgets
+
+directory = '~/jetbot'
+directory = os.path.expanduser(directory)
+sys.path.append(directory)
 
 
 #print data array
