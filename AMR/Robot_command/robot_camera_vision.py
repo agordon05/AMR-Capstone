9409b58b7f945c0819b2d82eb51424ac36ef3216
James Perry <jp28543@georgiasouthern.edu>
2023-10-30 17:58:30 -0400

    directory for importing jetbot module implemented

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -15,3 +15,5 @@ camera = Camera.instance(width=224, height=224)
 image = widgets.Image(format='jpeg', width=224, height=224)  # this width and height doesn't necessarily have to match the camera
 
 camera_link = traitlets.dlink((camera, 'value'), (image, 'value'), transform=bgr8_to_jpeg)
+#print data array
+#cv2.imwrite(image, mode = 'RGB')
