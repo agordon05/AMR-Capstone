4da722540a110e6e2ca2a0b79403b2a061f65c52
James Perry <jp28543@georgiasouthern.edu>
2023-10-30 19:28:32 -0400

    testing unknown error with numpy

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -1,7 +1,7 @@
 import os
 import sys
-#import robot_startup
-#from robot_startup import robot
+import robot_startup
+from robot_startup import robot
 import numpy
 import cv2
 import traitlets
@@ -11,8 +11,6 @@ directory = '~/jetbot'
 directory = os.path.expanduser(directory)
 sys.path.append(directory)
 from jetbot import Camera
-
-print("numpy")
 
 #print data array
 #cv2.imwrite(image, mode = 'RGB')
