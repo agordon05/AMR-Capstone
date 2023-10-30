4ba45f2b79361b7dbe14294ba1e7e2a20b63ea82
James Perry <jp28543@georgiasouthern.edu>
2023-10-30 19:45:49 -0400

    testing camera configuration with default cv2 functions

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -1,12 +1,9 @@
 import os
 import sys
-import robot_startup
-from robot_startup import robot
 import numpy
 import cv2
 import traitlets
 import ipywidgets.widgets as widgets
-# import gi
 
 # directory = '~/jetbot'
 # directory = os.path.expanduser(directory)
