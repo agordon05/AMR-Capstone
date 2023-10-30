520f9df609a1f161d9a9c2b8e7067b26bf9a3248
James Perry <jp28543@georgiasouthern.edu>
2023-10-30 18:34:34 -0400

    filler

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -2,11 +2,12 @@ import os
 import sys
 import robot_startup
 #from robot_startup import robot
-import numpy
+import numpy as np
 import cv2
 import traitlets
 import ipywidgets.widgets as widgets
 
+cap = cv2.VideoCapture(0)
 
 #print data array
 #cv2.imwrite(image, mode = 'RGB')
