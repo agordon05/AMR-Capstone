7a8df594c6b61a8beca495977626ccdcbabd2de0
James Perry <jp28543@georgiasouthern.edu>
2023-10-30 18:14:24 -0400

    filler

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -1,20 +1,12 @@
 import os
 import sys
 import robot_startup
-from robot_startup import robot
+#from robot_startup import robot
 import numpy
 import cv2
 import traitlets
 import ipywidgets.widgets as widgets
 
-directory = robot_startup.directory
-sys.path.append(directory)
 
-from jetbot import Camera, bgr8_to_jpeg
-
-camera = Camera.instance(width=224, height=224)
-image = widgets.Image(format='jpeg', width=224, height=224)  # this width and height doesn't necessarily have to match the camera
-
-camera_link = traitlets.dlink((camera, 'value'), (image, 'value'), transform=bgr8_to_jpeg)
 #print data array
 #cv2.imwrite(image, mode = 'RGB')
