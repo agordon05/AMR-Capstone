56fa4468d335ade783f557180e39247588a7b50f
agordon05 <100168379+agordon05@users.noreply.github.com>
2023-10-30 20:59:06 -0400

    Live image feed test
    first bit of code in one of the jupyter notebooks

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -1,15 +1,26 @@
 import os
 import sys
-import numpy
-import cv2
+# import numpy
+# import cv2
+# import traitlets
+# import ipywidgets.widgets as widgets
+directory = '~/jetbot'
+directory = os.path.expanduser(directory)
+sys.path.append(directory)
+# from jetbot import Camera #this is part that gives numpy error
 import traitlets
 import ipywidgets.widgets as widgets
+from IPython.display import display
+from jetbot import Camera, bgr8_to_jpeg
 
+
+# should only display a live image feed
 def method():
-    directory = '~/jetbot'
-    directory = os.path.expanduser(directory)
-    sys.path.append(directory)
-    from jetbot import Camera #this is part that gives numpy error
+    camera = Camera.instance(width=224, height=224)
+    image = widgets.Image(format='jpeg', width=224,
+                          height=224)  # this width and height doesn't necessarily have to match the camera
+    camera_link = traitlets.dlink((camera, 'value'), (image, 'value'), transform=bgr8_to_jpeg)
+    display(image)
 
 # capture = cv2.VideoCapture(0)
 #
--- AMR/main.py
+++ AMR/main.py
@@ -1,12 +1,13 @@
 from Robot_command import robot_startup
-
+from Robot_command import robot_camera_vision
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 # import Robot.robot_startup
 
-
-robot_startup.run()
+# at the moment it should only display a live image feed
+robot_camera_vision.method()
+# robot_startup.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
