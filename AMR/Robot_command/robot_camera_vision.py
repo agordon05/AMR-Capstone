a1e81131c04a35e068001cae825e307246e5b32c
agordon05 <100168379+agordon05@users.noreply.github.com>
2023-11-14 11:51:37 -0500

    Update robot_camera_vision.py
    set image to robot object as byte[], removed creating jpeg file, created get method for qr scanner to grab the image as is

--- AMR/Robot_command/robot_camera_vision.py
+++ AMR/Robot_command/robot_camera_vision.py
@@ -15,40 +15,49 @@ import traitlets
 import ipywidgets.widgets as widgets
 from IPython.display import display
 from jetbot import Camera, bgr8_to_jpeg
+from Model import robot as bot
 
-dir = 'dataset/imageFeed'
-file_name = 'image.jpg'
-sleep_time = 5
+# dir = 'dataset/imageFeed'
+# file_name = 'image.jpg'
+sleep_time = 2
+__image = None
+
+def get_image():
+    return __image
 
 #detector = cv2.QRCodeDetector
 
+# converts image to byte[] and sends it to the robot object
+def send_to_robot():
+    if __image:
+        image_in_byte = __image.value
+        bot.set_image(image_in_byte)
+
 # should only display a live image feed
-def method():
+def run():
+    global __image
     camera = Camera.instance(width=224, height=224)
-    image = widgets.Image(format='jpeg', width=224,
+    __image = widgets.Image(format='jpeg', width=224,
                           height=224)  # this width and height doesn't necessarily have to match the camera
-    camera_link = traitlets.dlink((camera, 'value'), (image, 'value'), transform=bgr8_to_jpeg)
+    camera_link = traitlets.dlink((camera, 'value'), (__image, 'value'), transform=bgr8_to_jpeg)
     display(image)
 
-    try:
-        os.makedirs(dir)
-    except FileExistsError:
-        print('Directory not created because it already exists')
+    # try:
+    #     os.makedirs(dir)
+    # except FileExistsError:
+    #     print('Directory not created because it already exists')
 
     while True:
         time.sleep(sleep_time)  # currently only captures images every [sleep_time] seconds
-        save_image(dir, image)
+        # save_image(dir, image)
+        send_to_robot()
 
-# ^ convert image to grayscale
-#
-#
-#
-#
 
-def save_image(directory, image):
-    image_path = os.path.join(directory, file_name)
-    with open(image_path, 'wb') as f:
-        f.write(image.value)
+# def save_image(directory, image):
+#     image_path = os.path.join(directory, file_name)
+#     with open(image_path, 'wb') as f:
+#         f.write(image.value)
+
 
 
 
