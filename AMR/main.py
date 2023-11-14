aacb205a6ac4f5b5f66b95fe0c93cff98824d8f6
agordon05 <100168379+agordon05@users.noreply.github.com>
2023-11-14 12:01:17 -0500

    Update main.py

--- AMR/main.py
+++ AMR/main.py
@@ -1,5 +1,5 @@
-# from Robot_command import robot_startup
-from Robot_command import robot_camera_vision
+from Robot_command import robot_startup
+# from Robot_command import robot_camera_vision
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
@@ -7,7 +7,7 @@ from Robot_command import robot_camera_vision
 # import Robot.robot_startup
 
 # at the moment it should only display a live image feed
-robot_camera_vision.method()
-# robot_startup.run()
+# robot_camera_vision.method()
+robot_startup.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
