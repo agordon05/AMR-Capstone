ec6b1f35fa27fd505282b678c162375b3d0cd226
Matthew Gordon <mg18667@georgiasouthern.edu>
2023-11-27 17:57:32 -0500

    base64 changes for sending images

--- AMR/Model/mock_robot.py
+++ AMR/Model/mock_robot.py
@@ -1,3 +1,6 @@
+from PIL import Image
+import io
+import base64
 # Mock data
 mock_robot_data = {
     'id': 1,
@@ -9,11 +12,23 @@ mock_robot_data = {
     'x_destination': 150.0,
     'y_destination': 150.0,
     'qrScan': 'mockQRCode',
-    'loggerList': ['Log1', 'Log2'],
+    'loggerList': ['Log1'],
     'image': None  # Example byte array for image
 }
 
+def create_mock_image_base64():
+    image = Image.new('RGB', (100, 100), color = 'red')  # A 100x100 red image
+    
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+
+    return base64.b64encode(img_byte_arr).decode('utf-8')
+
+mock_robot_data['image'] = create_mock_image_base64()
+
 def to_json():
+    mock_robot_data['image'] = create_mock_image_base64()
     return mock_robot_data
 
 def get_image():
